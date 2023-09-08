dbbdf746ae97664ccddc32e2e7b7322f27ff1102
shineishukla <shinei27shukla@gmail.com>
2023-09-08 17:40:06 +0530

    made changes

--- index.js
+++ index.js
@@ -22,6 +22,40 @@ $(".menu-items a").click(function () {
 });
 
 
+//add to cart function
+
+// Function to handle adding items to the cart
+function addToCart(productName, productPrice) {
+  // You can use localStorage, cookies, or a server to store cart information.
+  // For this example, we'll use localStorage.
+  
+  // Get the current cart count from localStorage
+  let cartCount = localStorage.getItem("cartCount") || 0;
+  
+  // Increment the cart count
+  cartCount = parseInt(cartCount) + 1;
+  
+  // Store the updated cart count in localStorage
+  localStorage.setItem("cartCount", cartCount);
+  
+  // Update the cart count displayed on the page
+  document.getElementById("cart-count").textContent = cartCount;
+  
+  // You can also store product information in the cart if needed.
+}
+
+// Get all "Add to Cart" buttons and add click event listeners
+const addToCartButtons = document.querySelectorAll(".add-to-cart");
+addToCartButtons.forEach(button => {
+  button.addEventListener("click", () => {
+      const productName = button.getAttribute("data-product");
+      const productPrice = button.getAttribute("data-price");
+      addToCart(productName, productPrice);
+  });
+});
+
+// Initialize the cart count on page load
+document.getElementById("cart-count").textContent = localStorage.getItem("cartCount") || 0;
 
 
 
